18e02b131e34d1baa6c3566ee41a77a044e43f54
Filmonaa <fila.mesfin@gmail.com>
2023-01-10 02:05:51 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,7 +5,6 @@
  * argstostr - concatenates all the arguments of your program
  * @ac: arguments count
  * @av: arguments vector
- * 
  * Return: a pointer to a new string, or NULL if it fails
  */
 
