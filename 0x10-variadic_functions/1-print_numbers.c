c2ff3d08ef2fbaba03c2d730a6ffcdced51ad874
Filmonaa <fila.mesfin@gmail.com>
2023-01-19 11:02:43 -0800

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  * Return: Always 0
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...);
 {
 	unsigned int i;
 	va_list ap;
